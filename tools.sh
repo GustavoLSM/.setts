8a4f398c9dae7559c6084eb67596458cbbecf32e
Gustavo Sousa <lsousa.gus@outlook.com>
2025-07-04 23:49:59 -0300

    Update tool installation script

--- tools.sh
+++ tools.sh
@@ -8,19 +8,19 @@
 yay -S starship stow fastfetch \
     cifs-utils samba openvpn \
     freerdp remmina teamviewer \
-    winbox keepassxc libreoffice-fresh
+    winbox keepassxc libreoffice-fresh \
+    rxvt-unicode-truecolor-wide-glyphs
 
 # Desktop experience
 yay -S sxwm-git thunar betterlockscreen \
-    gvfs maim xclip
+    gvfs maim xclip dmenu
 
 mkdir -p ~/.local/clones/
-git clone https://git.suckless.org/st ~/.local/clones/st
 git clone https://github.com/uint23/sxbar ~/.local/clones/sxbar
 
 # Appearance
 yay -S nwg-look qt5ct nitrogen \
-    noto-fonts-extra noto-fonts-emoji ttf-font-awesome ttf-bigblueterminal-nerd
+    noto-fonts-extra noto-fonts-emoji ttf-font-awesome ttf-terminus-nerd
 
 echo "QT_QPA_PLATFORMTHEME=qt5ct" | sudo tee -a /etc/environment
 echo "XCURSOR_SIZE=20" | sudo tee -a /etc/environment
